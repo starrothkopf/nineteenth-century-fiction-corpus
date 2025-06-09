f5cf589ed4d7094767af08957d2ffd54bb199791
star rothkopf <rothkopfstar@gmail.com>
2025-06-09 12:50:33 -0500

    basic classifier

---
+++ .history/classifier_20250609122017.py
@@ -0,0 +1,9 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv('noveltm_ef.csv')
+
+X = df[['page_count', 'nonficprob', 'pub_date']]  
+y = df['genre'].str.contains("fiction", case=False).astype(int)  # 1 = fiction, 0 = not
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
